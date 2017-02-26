51322c988d431cbf7eaa9e35a4364baeacba617c
Maurice Bos <m-ou.se@m-ou.se>
2017-02-26 23:39:07 +0100

    For optional and string_view, pre C++17: Check for experimental first.
    libstdc++ has (intentionally) broken <optional> and <string_view>
    available in C++14 mode, which we don't want to include.

--- include/mstd/optional.hpp
+++ include/mstd/optional.hpp
@@ -1,17 +1,17 @@
 #pragma once
-#if __has_include(<optional>)
-#include <optional>
-namespace mstd {
-using std::optional;
-using std::nullopt;
-using std::make_optional;
-}
-#elif __has_include(<experimental/optional>)
+#if __cplusplus <= 201402L && __has_include(<experimental/optional>)
 #include <experimental/optional>
 namespace mstd {
 using std::experimental::optional;
 using std::experimental::nullopt;
 using std::experimental::make_optional;
+}
+#elif __has_include(<optional>)
+#include <optional>
+namespace mstd {
+using std::optional;
+using std::nullopt;
+using std::make_optional;
 }
 #else
 #error Missing <optional>
--- include/mstd/string_view.hpp
+++ include/mstd/string_view.hpp
@@ -1,15 +1,15 @@
 #pragma once
-#if __has_include(<string_view>)
-#include <string_view>
-namespace mstd {
-using std::string_view;
-using std::basic_string_view;
-}
-#elif __has_include(<experimental/string_view>)
+#if __cplusplus >= 201402L && __has_include(<experimental/string_view>)
 #include <experimental/string_view>
 namespace mstd {
 using std::experimental::string_view;
 using std::experimental::basic_string_view;
+}
+#elif __has_include(<string_view>)
+#include <string_view>
+namespace mstd {
+using std::string_view;
+using std::basic_string_view;
 }
 #else
 #error missing <string_view>
